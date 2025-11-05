31876b2737e5a4999b8e5125531793b1ac1bf098
Este98 <este98@hotmail.fr>
2025-11-05 10:16:14 +0100

    feat: router

---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Meteo from "../views/Meteo.vue";
+import APropos from "../views/APropos.vue";
+import MentionsLegales from "../views/MentionsLegales.vue";
+
+const routes = [
+  { path: '/', name: 'Meteo', component: Meteo },
+  {path: '/a-propos', name: 'APropos', component: APropos },
+  {path: '/mentions-legales', name: 'MentionsLegales', component: MentionsLegales }
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
